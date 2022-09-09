76bebb5e9e92f993181741bdea59250db50e9ddd
Ali <malisheikhg772@gmail.com>
2022-09-09 17:11:04 +0500

    Upload Project Website

---
+++ js/java.js
@@ -0,0 +1,17 @@
+var navbar = document.getElementById("navbar");
+
+function showmenu() {
+    navbar.style.width = "200px";
+}
+
+function hidemenu() {
+    navbar.style.width = "0px";
+}
+
+
+
+
+window.addEventListener("load", function(){
+        /* -----------------------------  animation on scroll-------------------------------- */
+         AOS.init();
+});
---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Pizza",
+    price: 20.00,
+    instock: 100,
+    description:
+      "So Tasty And Delicious.",
+    imgSrc:"./images1/g-1.jpg ",
+  },
+  {
+    id: 1,
+    name: "pizza",
+    price: 24.99,
+    instock: 43,
+    description:
+    "So Tasty And Delicious.",
+    imgSrc: "./images1/g-2.jpg ",
+  },
+  {
+    id: 2,
+    name: "pizza",
+    price: 19.99,
+    instock: 10,
+    description:
+    "So Tasty And Delicious.",
+    imgSrc: "./images1/g-3.jpg",
+  },
+  {
+    id: 3,
+    name: "pizza",
+    price: 25.99,
+    instock: 5,
+    description:
+    "So Tasty And Delicious.",
+    imgSrc: "./images1/g-4.jpg",
+  },
+  {
+    id: 4,
+    name: "pizza",
+    price: 29.99,
+    instock: 4,
+    description:
+    "So Tasty And Delicious.",
+    imgSrc: "./images1/g-5.jpg",
+  },
+  {
+    id: 5,
+    name: "pizza",
+    price: 39.99,
+    instock: 40,
+    description:
+    "So Tasty And Delicious.",
+    imgSrc: "./images1/g-7.jpg",
+  },
+];
